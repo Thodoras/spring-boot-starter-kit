94af5728b3d943de35f7e1047471f99aed68b2b1
geokyr <gkyriakou@tripsta.com>
2016-12-21 15:43:01 +0200

    tests should explicitly use the test profile

--- application/web/src/test/java/com/sbsk/web/controllers/UserControllerTests.java
+++ application/web/src/test/java/com/sbsk/web/controllers/UserControllerTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserControllerTests {
 
   private static final String GET_RESPONSE = "hey Nikos";
